cd91bdd23f073cfd80580f049e5adf79b4980cea
AlexanderGG-0520 <uket.panda.1st@gmail.com>
2025-04-21 20:33:24 +0900

    feat: 次のステージの準備処理を修正し、2ステージ以降にお題を引く機能を追加

--- commands/utility/nextstage.ts
+++ commands/utility/nextstage.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { ChatInputCommandInteraction } from 'discord.js';
-import { hasGame, prepareNextStage } from '../../utils/gameManager';
+import { hasGame, prepareNextStage, drawNewTopic } from '../../utils/gameManager';
 
 export const data = new SlashCommandBuilder()
   .setName('nextstage')
@@ -12,11 +12,22 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     await interaction.reply('ゲームが開始されていません。`/start` で開始してください。');
     return;
   }
+
+  // ステージの進行準備
   const { stage, lives, momoCard } = prepareNextStage(gid);
+
   let msg = `ステージ **${stage}** の準備が完了しました。\nライフ: **${lives}**\n`;
+
+  // 2ステージ以降はお題を引く
+  if (stage >= 2) {
+    const topic = drawNewTopic(gid);
+    msg += `お題: **${topic}**\n`;
+  }
+
   if (momoCard !== undefined) {
     msg += `モモちゃん発見！ナンバー **${momoCard}** を場の脇に置きました。\n`;
   }
+
   msg += '各プレイヤーは `/draw` で配られた手札をご確認ください。';
   await interaction.reply(msg);
 }
